57ce8fe2b8145e154eed97eef84b8ada5992893b
AstronautNon <1962637260@qq.com>
2026-04-19 20:06:10 +0800

    提取小时部份并建立新列hour

--- demo.py
+++ demo.py
@@ -10,3 +10,5 @@ print("\n--- 数据基本信息 ---")
 print(df.info())
 #交易时间 列的类型更改为datatime
 df['交易时间'] = pd.to_datetime(df['交易时间'], errors='coerce')
+# 提取“交易时间”列的小时部分，并赋值给新列 'hour'
+df['hour'] = df['交易时间'].dt.hour
